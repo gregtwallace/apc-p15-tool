90c2baf343a7885aabe7e2f1ed4ce0ee21b45050
Greg T. Wallace <greg@gregtwallace.com>
2024-02-03 14:07:26 -0500

    local build: ensure cgo is disabled

--- build.ps1
+++ build.ps1
@@ -5,26 +5,32 @@ $outDir = Join-Path -Path $scriptDir -ChildPath "/_out"
 # Windows x64
 $env:GOARCH = "amd64"
 $env:GOOS = "windows"
+$env:CGO_ENABLED = 0
 go build -o $outDir/apc-p15-tool-amd64.exe ./cmd/tool
 
 $env:GOARCH = "amd64"
 $env:GOOS = "windows"
+$env:CGO_ENABLED = 0
 go build -o $outDir/apc-p15-install-amd64.exe ./cmd/tool
 
 # Linux x64
 $env:GOARCH = "amd64"
 $env:GOOS = "linux"
+$env:CGO_ENABLED = 0
 go build -o $outDir/apc-p15-tool-amd64 ./cmd/tool
 
 $env:GOARCH = "amd64"
 $env:GOOS = "linux"
+$env:CGO_ENABLED = 0
 go build -o $outDir/apc-p15-install-amd64 ./cmd/install_only
 
 # Linux arm64
 $env:GOARCH = "arm64"
 $env:GOOS = "linux"
+$env:CGO_ENABLED = 0
 go build -o $outDir/apc-p15-tool-arm64 ./cmd/tool
 
 $env:GOARCH = "arm64"
 $env:GOOS = "linux"
+$env:CGO_ENABLED = 0
 go build -o $outDir/apc-p15-install-arm64 ./cmd/install_only
